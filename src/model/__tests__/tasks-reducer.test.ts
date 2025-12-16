690f7460456f494f5880efbb8bb9020bcdab989e
Lion77777 <leon.avetisyan.1993@mail.ru>
2025-12-17 03:17:54 +0400

    Implement test for task creating logic

--- src/model/__tests__/tasks-reducer.test.ts
+++ src/model/__tests__/tasks-reducer.test.ts
@@ -1,4 +1,18 @@
+import { v1 } from "uuid"
+import { createTaskAC, tasksReducer } from "../tasks-reducer"
+import { Task } from "../../App"
 
-export const tasksReducer = (state, action) => {
+test('correct task should be created', () => {
+    const startState: Task[] = [
+        { id: v1(), status: false, title: 'Тестовое задание' },
+        { id: v1(), status: true, title: 'Прекрасный код' },
+        { id: v1(), status: false, title: 'Покрытие тестами' }
+    ]
 
-}
+    const title = "New Task"
+
+    const endState = tasksReducer(startState, createTaskAC(title))
+
+    expect(endState.length).toBe(4)
+    expect(endState[0].title).toBe(title)
+})
