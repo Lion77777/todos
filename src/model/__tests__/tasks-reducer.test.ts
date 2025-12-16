56c21635813704d757740e29dd8e09730bb1a469
Lion77777 <leon.avetisyan.1993@mail.ru>
2025-12-17 03:32:14 +0400

    Implement test for task's status changing logic

--- src/model/__tests__/tasks-reducer.test.ts
+++ src/model/__tests__/tasks-reducer.test.ts
@@ -1,18 +1,30 @@
 import { v1 } from "uuid"
-import { createTaskAC, tasksReducer } from "../tasks-reducer"
+import { changeTaskStatusAC, createTaskAC, tasksReducer } from "../tasks-reducer"
 import { Task } from "../../App"
 
-test('correct task should be created', () => {
-    const startState: Task[] = [
+let startState: Task[]
+
+beforeEach(() => {
+startState = [
         { id: v1(), status: false, title: 'Тестовое задание' },
         { id: v1(), status: true, title: 'Прекрасный код' },
         { id: v1(), status: false, title: 'Покрытие тестами' }
     ]
+})
 
+test('correct task should be created', () => {
     const title = "New Task"
 
     const endState = tasksReducer(startState, createTaskAC(title))
 
     expect(endState.length).toBe(4)
     expect(endState[0].title).toBe(title)
+})
+
+test('correct task should change its status', () => {
+    const id = startState[0].id
+    const endState = tasksReducer(startState, changeTaskStatusAC({id, status: true}))
+
+    expect(endState[0].status).toBeTruthy()
+    expect(endState.length).toBe(startState.length)
 })
