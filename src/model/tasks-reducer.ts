7151d5c8687e4c346343833d369a5da527549bd2
Lion77777 <leon.avetisyan.1993@mail.ru>
2025-12-17 03:38:29 +0400

    Implement completed tasks removing logic

--- src/model/tasks-reducer.ts
+++ src/model/tasks-reducer.ts
@@ -15,9 +15,14 @@ export const tasksReducer = (state = initialState, action: Actions) => {
         }
         case 'change_task_status': {
             const {id, status} = action.payload
-            const updatedTask = state.map(task => task.id === id ? {...task, status} : task)
+            const updatedTasks = state.map(task => task.id === id ? {...task, status} : task)
 
-            return [...updatedTask]
+            return [...updatedTasks]
+        }
+        case 'clear_completed_tasks': {
+            const updatedTasks = state.filter(task => task.status !== action.payload.status)
+
+            return [...updatedTasks]
         }
     }
     
@@ -31,8 +36,13 @@ export const changeTaskStatusAC = (payload: {id: string, status: boolean}) => {
     return {type: 'change_task_status', payload} as const
 }
 
+export const clearCompletedTasksAC = (status: boolean) => {
+    return {type: 'clear_completed_tasks', payload: {status}} as const
+}
+
 
 export type createTaskAction = ReturnType<typeof createTaskAC>
 export type changeTaskStatusAction = ReturnType<typeof changeTaskStatusAC>
+export type clearCompletedTasksAction = ReturnType<typeof clearCompletedTasksAC>
 
-type Actions = createTaskAction | changeTaskStatusAction
+type Actions = createTaskAction | changeTaskStatusAction | clearCompletedTasksAction
